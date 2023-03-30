db6157ce2fafed7d2e6e0f3f83a97ffcc48cdf6a
Leonardo-Pico <63673074+Leonardo-Pico@users.noreply.github.com>
2023-03-29 23:22:02 -0500

    Videos

--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -141,7 +141,7 @@
     .row
       figure.mb-5.mt-3
         .video
-         iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+         iframe(width="560" height="315" src="https://www.youtube.com/embed/6cnuU7u86Xw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
     .clr-background-4.mt-3
       .row.justify-content-around.align-items-center
